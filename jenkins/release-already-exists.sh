4e3d49fe4b7dbef419826b2c296f3a0f46eb2579
gounthar <gounthar@gmail.com>
2022-07-19 19:17:40 +0200

    Won't do the cut if the variable is already empty after the `grep`
    I'm not a surgeon anyway.

--- jenkins/release-already-exists.sh
+++ jenkins/release-already-exists.sh
@@ -7,7 +7,7 @@ versionCode=$(./gradlew printVersion | grep "Version code:" | grep -o '[^ ]*$')
 # echo "Release version code: ${versionCode}"
 
 echo $GITHUB_CREDENTIALS_PSW | gh auth login --with-token
-existingRelease=$(gh release list | grep ${versionName} | cut -d$'\t' -f 1 | cut -c 2-)
-# echo "Existing release is ${existingRelease}"
-result=$([ -z "$existingRelease" ] && echo "false" || echo "true")
-echo "${result}"
+lookAlikeExistingRelease=$(gh release list | grep $versionName)
+result=$([ -z "$lookAlikeExistingRelease" ] && echo "" || echo "$lookAlikeExistingRelease" | cut -d$'\t' -f 1 | cut -c 2-)
+
+[ -z "$result" ] && echo "false" || echo "true"
