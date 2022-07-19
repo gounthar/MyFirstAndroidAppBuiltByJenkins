b506e4e7ca425946b1c0e2ad54689482713027ed
gounthar <gounthar@gmail.com>
2022-07-19 19:08:23 +0200

    Now uses `./gradlew printVersion` to find if we have an existing release of the same version.

--- jenkins/release-already-exists.sh
+++ jenkins/release-already-exists.sh
@@ -1,14 +1,13 @@
- #!/bin/sh -fvx
+ #!/bin/sh
 
-versionName=$(grep versionName app/build.gradle | cut -d '"' -f 2)
+versionName=$(./gradlew printVersion | grep "Version name:" | cut -d ' ' -f 3)
 # echo "Release version: ${versionName}"
 
-versionCode=$(grep versionCode app/build.gradle | grep -o '[^ ]*$')
+versionCode=$(./gradlew printVersion | grep "Version code:" | grep -o '[^ ]*$')
 # echo "Release version code: ${versionCode}"
 
 echo $GITHUB_CREDENTIALS_PSW | gh auth login --with-token
 existingRelease=$(gh release list | grep ${versionName} | cut -d$'\t' -f 1 | cut -c 2-)
 # echo "Existing release is ${existingRelease}"
-# echo "${existingRelease}"
 result=$([ -z "$existingRelease" ] && echo "false" || echo "true")
 echo "${result}"
