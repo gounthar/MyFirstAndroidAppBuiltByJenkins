f38c4ac12973acde99ddaba4fc4b77f6f73ee1eb
gounthar <gounthar@gmail.com>
2022-07-20 14:59:36 +0200

    Better handling of whitespace.

--- jenkins/release-already-exists.sh
+++ jenkins/release-already-exists.sh
@@ -1,13 +1,14 @@
  #!/bin/sh
 
-versionName=$(./gradlew printVersion | grep "Version name:" | cut -d ' ' -f 3)
+versionName=$(./gradlew printVersion | grep "Version name:" | cut -d ' ' -f 3 | sed -e 's/^[[:space:]]*//')
 # echo "Release version: ${versionName}"
 
-versionCode=$(./gradlew printVersion | grep "Version code:" | grep -o '[^ ]*$')
+versionCode=$(./gradlew printVersion | grep "Version code:" | grep -o '[^ ]*$' | sed -e 's/^[[:space:]]*//')
 # echo "Release version code: ${versionCode}"
 
-echo $GITHUB_CREDENTIALS_PSW | gh auth login --with-token
-lookAlikeExistingRelease=$(gh release list | grep $versionName)
-result=$([ -z "$lookAlikeExistingRelease" ] && echo "" || echo "$lookAlikeExistingRelease" | cut -d$'\t' -f 1 | cut -c 2-)
+# echo $GITHUB_CREDENTIALS_PSW | gh auth login --with-token
+lookAlikeExistingRelease=$(gh release list | grep $versionName | cut -d$'\t' -f 1 | cut -c 2-)
+# echo "Existing release for $versionName : $lookAlikeExistingRelease"
 
+result=$([ -z "$lookAlikeExistingRelease" ] && echo "" || echo "$lookAlikeExistingRelease")
 [ -z "$result" ] && echo -n "false" || echo -n "true"
