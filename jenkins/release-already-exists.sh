47127313342419ac3c1bbc66167d1269825b3315
gounthar <gounthar@gmail.com>
2022-07-20 15:33:46 +0200

    Who commented the credentials login?

--- jenkins/release-already-exists.sh
+++ jenkins/release-already-exists.sh
@@ -6,7 +6,7 @@ versionName=$(./gradlew printVersion | grep "Version name:" | cut -d ' ' -f 3 | 
 versionCode=$(./gradlew printVersion | grep "Version code:" | grep -o '[^ ]*$' | sed -e 's/^[[:space:]]*//')
 # echo "Release version code: ${versionCode}"
 
-# echo $GITHUB_CREDENTIALS_PSW | gh auth login --with-token
+echo $GITHUB_CREDENTIALS_PSW | gh auth login --with-token
 lookAlikeExistingRelease=$(gh release list | grep $versionName | cut -d$'\t' -f 1 | cut -c 2-)
 # echo "Existing release for $versionName : $lookAlikeExistingRelease"
 
