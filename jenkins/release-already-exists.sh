91bf4a46f4a2235ea1686b9588687e9e7f8e949d
gounthar <gounthar@gmail.com>
2022-07-21 12:21:59 +0200

    Because tabs became spaces with the ALPHA release.
    Go figure...

--- jenkins/release-already-exists.sh
+++ jenkins/release-already-exists.sh
@@ -7,12 +7,12 @@ versionCode=$(./gradlew printVersion | grep "Version code:" | grep -o '[^ ]*$' |
 # echo "Release version code: ${versionCode}"
 
 echo $GITHUB_CREDENTIALS_PSW | gh auth login --with-token
-existingRelease=$(gh release list | grep -F $versionName)
+existingRelease=$(gh release list | grep -F $versionName | head -1)
 lookAlikeExistingRelease=
 
 [ -z "$existingRelease" ] || {
 # echo "Release $versionName already exists"
-  lookAlikeExistingRelease=$(echo $existingRelease | cut -d$'\t' -f 1 | cut -c 2-)
+  lookAlikeExistingRelease=$(echo $existingRelease | cut -d$'\t' -f 1 | cut -d ' ' -f 1 | cut -c 2-)
 }
 # echo "Existing release for $versionName : $lookAlikeExistingRelease"
 
