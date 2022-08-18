28b380707307a812be5da033f57f5ebe0dce901a
gounthar <gounthar@gmail.com>
2022-08-18 17:38:50 +0200

    fix(docker) Specific image for the agent now

---
+++ jenkins/agent/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+# Pre Hooks
+# We could have something better by adjusting
+# settings in daemon.json (maybe in /etc/docker/daemon.json)
+# because of failed to start daemon: pid file found, ensure docker is not running or delete /var/run/docker.pid
+rm -rf /var/run/docker*
+dockerd --storage-driver=vfs --iptables=false &
+# Run default entrypoint
+exec /usr/local/bin/setup-sshd "$@"
