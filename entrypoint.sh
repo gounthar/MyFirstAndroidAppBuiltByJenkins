5094ddca70492af01bdc009cb74f8a0119de0e17
gounthar <gounthar@gmail.com>
2022-08-18 17:34:58 +0200

    fix(docker) We don't need DinD in the image that will be used by the agent.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -eux -o pipefail
 # We could have something better by adjusting
 # settings in daemon.json (maybe in /etc/docker/daemon.json)
 # because of failed to start daemon: pid file found, ensure docker is not running or delete /var/run/docker.pid
-rm -rf /var/run/docker*
-dockerd --storage-driver=vfs --iptables=false &
+# rm -rf /var/run/docker*
+# dockerd --storage-driver=vfs --iptables=false &
 # Run default entrypoint
 exec /usr/local/bin/setup-sshd "$@"
