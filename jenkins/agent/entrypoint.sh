d5e4d8941a33d5ba43d689d090a734cd0eca2f4f
Bruno Verachten <gounthar@gmail.com>
2022-08-22 16:02:22 +0000

    Had to change the mode for docker.sock

--- jenkins/agent/entrypoint.sh
+++ jenkins/agent/entrypoint.sh
@@ -7,12 +7,12 @@ set -eux -o pipefail
 dockersock_growner_id="$(stat -c "%g" /var/run/docker.sock)"
 if [[ "$dockersock_growner_id" == "0" ]]; then
   chown root:docker /var/run/docker.sock
-  chmod 660 /var/run/docker.sock
 else
   delgroup docker || true
   addgroup docker --gid "${dockersock_growner_id}"
 fi
 adduser jenkins docker
+chmod 660 /var/run/docker.sock
 
 # Run default entrypoint
 exec /usr/local/bin/setup-sshd "$@"
