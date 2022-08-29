549cb89adabae047363362625dc8b26a3d7f54b9
gounthar <gounthar@gmail.com>
2022-08-29 18:31:49 +0200

    fix(android) Test was not referencoing the right class.

--- app/src/androidTest/java/io/jenkins/mobile/example/myfirstbuiltbyjenkinsapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/jenkins/mobile/example/myfirstbuiltbyjenkinsapplication/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.myfirstbuiltbyjenkinsapplications", appContext.packageName)
+        assertEquals("io.jenkins.mobile.example.myfirstbuiltbyjenkinsapplication", appContext.packageName)
     }
 }
