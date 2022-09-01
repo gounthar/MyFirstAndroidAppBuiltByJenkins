3e249889e2b1ac434c93742f53eac4143a0df520
gounthar <gounthar@gmail.com>
2022-09-01 11:39:32 +0200

    feat(jenkins) The script now updates plugins.txt
    And creates a Pull Request by itself.

---
+++ jenkins/controller/update-plugins.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+JENKINS_HOST=admin:butler@jenkins:8080
+#JENKINS_HOST=admin:butler@localhost:8080
+curl -sSL "http://$JENKINS_HOST/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g'|sed 's/ /:/' > /tmp/jenkins_plugins.txt
+sort -o /tmp/sorted_jenkins_plugins.txt /tmp/jenkins_plugins.txt
+sed -i '1s/^/# See https:\/\/github.com\/jenkinsci\/docker#usage-1\n/' /tmp/sorted_jenkins_plugins.txt
+diff -u ./plugins.txt /tmp/sorted_jenkins_plugins.txt > /tmp/jenkins_plugins.diff
+if [ -s /tmp/jenkins_plugins.diff ]; then
+    echo "Plugins have changed, updating plugins.txt"
+    cp /tmp/sorted_jenkins_plugins.txt ./plugins.txt
+    git add ./plugins.txt
+    # Get current branch name
+    branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
+    branch_name="(unnamed branch)"     # detached HEAD
+    branch_name=${branch_name##refs/heads/}
+    echo "Working on branch $branch_name"
+    new_branch_name="update-$branch_name/"$(sha1sum /tmp/jenkins_plugins.diff | cut -d " " -f1)
+    git switch -c "${new_branch_name}" -m
+    git commit -m "Update plugins.txt"
+    git push --set-upstream origin "${new_branch_name}"
+    # Now use gh to create a pull request from new_branch_name to branch_name
+    gh pr create -B "$branch_name" -t "Update plugins.txt" -b "Update plugins.txt"
+    git switch "${branch_name}"
+else
+    echo "Plugins have not changed"
+fi
