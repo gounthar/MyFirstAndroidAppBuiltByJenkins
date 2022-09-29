c2d3e5c42a7a1225f4b72392d7460e791493d043
gounthar <gounthar@gmail.com>
2022-09-29 18:19:29 +0200

    feat(docker) First version of the STF Farm using the emulator.

---
+++ jenkins/adb-connector/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+set -eux -o pipefail
+
+# Pre Hooks
+# Prepare Permissions by adding jenkins to the group owner of /var/run/docker.sock (this GID changes on different hosts/docker engines)
+dockersock_growner_id="$(stat -c "%g" /var/run/docker.sock)"
+if [[ "$dockersock_growner_id" == "0" ]]; then
+  chown root:docker /var/run/docker.sock
+else
+  delgroup docker || true
+  addgroup docker --gid "${dockersock_growner_id}"
+fi
+adduser jenkins docker
+chmod 660 /var/run/docker.sock
+
+# Run default entrypoint
+exec /bin/sh -c "$@"
